929534466a302e6ecefa904759c34fd91e5e8885
honza <honza@Home1B>
2018-07-05 09:35:41 -0700

    Added Contact and About pages

--- application/controllers/Public_ctl.php
+++ application/controllers/Public_ctl.php
@@ -16,7 +16,13 @@ class Public_ctl extends CI_Controller {
 	
 	public function contact() {		
 		$this->load->view('template/header_public_contact');
-		
+		$this->load->view('public/contact_view');
+		$this->load->view('template/footer');
+	}
+	
+	public function about() {
+		$this->load->view('template/header_public_about');
+		$this->load->view('public/about_view');
 		$this->load->view('template/footer');
 	}
 }
---
+++ application/views/public/contact_view.php
@@ -0,0 +1,89 @@
+        <div id="heading-breadcrumbs" class="no-mb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <h1>Contact</h1>
+                    </div>
+                    <div class="col-md-5">
+                        <ul class="breadcrumb">
+                            <li><?php echo anchor('Public_ctl', 'Home');?></li>
+                            <li>Contact</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <section>
+
+            <div id="map" class="with-border">
+
+            </div>
+
+        </section>
+
+        <div id="content">
+            <div class="container" id="contact">
+
+                <section>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="heading">
+                                <h3>Contact form</h3>
+                            </div>
+
+                            <form>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="firstname">Firstname</label>
+                                            <input type="text" class="form-control" id="firstname">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="lastname">Lastname</label>
+                                            <input type="text" class="form-control" id="lastname">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="text" class="form-control" id="email">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="subject">Subject</label>
+                                            <input type="text" class="form-control" id="subject">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label for="message">Message</label>
+                                            <textarea id="message" class="form-control"></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-12 text-center">
+                                      <br>
+                                        <button type="submit" class="btn btn-template-main"><i class="fa fa-envelope-o"></i> Send message</button>
+
+                                    </div>
+                                </div>
+                                <!-- /.row -->
+                            </form>
+                        </div>
+
+                        <div class="col-md-4">&nbsp;</div>
+
+                    </div>
+
+
+                </section>
+
+            </div>
+            <!-- /#contact.container -->
+        </div>
+        <!-- /#content -->
