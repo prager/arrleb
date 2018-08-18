b6f48fa77373b5fce33d1c3bd858e6a86aab6659
honza <honza@laptop3>
2018-08-18 16:51:27 +0200

    Added education route

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$route['default_controller'] = 'Welcome';
 $route['default_controller'] = 'Public_ctl';
 $route['about'] = 'Public_ctl/about';
+$route['education'] = 'Public_ctl/education';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
