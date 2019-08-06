6ffb084967c8e20676e136d0899c1cd432840158
honza <honza@Home1B>
2019-08-05 20:24:27 -0700

    Changed next event to Tech Class by MDARC

--- application/views/public/main_view.php
+++ application/views/public/main_view.php
@@ -86,11 +86,11 @@
                 <div class="row">
                     <div class="col-md-12">
                         <h3>Next Selected Event: </h3>
-                        <h2 class="text-uppercase">Introduction to DX</h2> 
+                        <h2 class="text-uppercase">Technician License Course</h2> 
                            <p class="lead mb-small">
                            By MDARC (Mount Diablo Amateur Radio Club)</p> 
-                           <p>Date: 08/01/2019</p>
-                        <p class="mb-small">For more details <?php echo anchor_popup('http://www.mdarc.org/activities/education/Classes', 
+                           <p>Dates: 08/15/2019 - 10/10/2019</p>
+                        <p class="mb-small">For more details <?php echo anchor_popup(base_url() . 'index.php/public_ctl/class_details/28', 
                             'click here')?>
                         --- More classes listed here <?php echo anchor_popup('education', 'here')?></p>
                     </div>
